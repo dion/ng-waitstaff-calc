ecd1c22521c7ca1675837edca04741f42498ef4d
Dion Yang <jellopops@gmail.com>
2015-07-20 18:23:35 -0700

    merge from routes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-	angular.module('myApp', [])
-	.controller('waitstaffCalc', function ($scope) {
-		$scope.mealData = {
+	angular.module('myApp', ['ngRoute', 'ngAnimate'])
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'home.html',
+			controller: 'HomeCtrl as home'
+		})
+		.when('/new-meal', {
+			templateUrl: 'new-meal.html',
+			controller: 'NewCtrl as newMeal'
+		})
+		.when('/my-earnings', {
+			templateUrl: 'my-earnings',
+			controller: 'EarningsCtrl as earnings'
+		})
+		.when('/error', {
+			template: '<p>Error - Page Not Found</p>'
+		})
+		.otherwise('/error');
+	}])
+	.run(function($rootScope, $location) {
+	    $rootScope.$on('$routeChangeError', function() {
+	        $location.path('/error');
+	    });
+	})
+	.controller('HomeCtrl', function ($scope) {
+
+	})
+	.controller('NewCtrl', function ($rootScope) {
+		$rootScope.mealData = {
 			basePrice: null,
 			taxRate: null,
 			tipPercentage: null,
@@ -9,36 +35,37 @@
 			total: 0
 		};
 
-		$scope.earningsInfo = {
+		$rootScope.earningsInfo = {
 			tipTotal: 0,
 			mealCount: 0,
 			avgTipPerMeal: 0
 		};
 
-		$scope.reset = function () {
-			$scope.clearObj($scope.mealData);
-			$scope.clearObj($scope.earningsInfo);
+		this.submit = function () {
+			var taxRateToDecimal = $rootScope.mealData.taxRate / 100,
+			    tipRateToDecimal = $rootScope.mealData.tipPercentage / 100,
+				taxAmount = $rootScope.mealData.basePrice * taxRateToDecimal,
+				tipAmount = $rootScope.mealData.basePrice * tipRateToDecimal;
+
+			$rootScope.mealData.subtotal = $rootScope.mealData.basePrice + taxAmount;
+			$rootScope.mealData.taxAmount = taxAmount;
+			$rootScope.mealData.tipAmount = tipAmount;
+			$rootScope.mealData.total = $rootScope.mealData.subtotal + $rootScope.mealData.tipAmount;
+
+			$rootScope.earningsInfo.tipTotal += $rootScope.mealData.tipAmount;
+			$rootScope.earningsInfo.mealCount++;
+			$rootScope.earningsInfo.avgTipPerMeal = $rootScope.earningsInfo.tipTotal / $rootScope.earningsInfo.mealCount;
+		};
+	})
+	.controller('EarningsCtrl', function ($rootScope) {
+		this.reset = function () {
+			this.clearObj($rootScope.mealData);
+			this.clearObj($rootScope.earningsInfo);
 		};
 
-		$scope.clearObj = function (obj) {
+		this.clearObj = function (obj) {
 			for (i in obj) {
 				obj[i] = 0;
 			}
-		};
-
-		$scope.submit = function () {
-			var taxRateToDecimal = $scope.mealData.taxRate / 100,
-			    tipRateToDecimal = $scope.mealData.tipPercentage / 100,
-				taxAmount = $scope.mealData.basePrice * taxRateToDecimal,
-				tipAmount = $scope.mealData.basePrice * tipRateToDecimal;
-
-			$scope.mealData.subtotal = $scope.mealData.basePrice + taxAmount;
-			$scope.mealData.taxAmount = taxAmount;
-			$scope.mealData.tipAmount = tipAmount;
-			$scope.mealData.total = $scope.mealData.subtotal + $scope.mealData.tipAmount;
-
-			$scope.earningsInfo.tipTotal += $scope.mealData.tipAmount;
-			$scope.earningsInfo.mealCount++;
-			$scope.earningsInfo.avgTipPerMeal = $scope.earningsInfo.tipTotal / $scope.earningsInfo.mealCount;
 		};
 	});
